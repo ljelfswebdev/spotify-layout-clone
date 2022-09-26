fdcdca4f9c80cf610cc35983b5a4c669755c42af
Lewis Jelfs <97512035+ljelfswebdev@users.noreply.github.com>
2022-09-26 18:58:45 +0100

    footer and nav

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Footer = () => {
-    const header1 = [ 'link1', 'link2', 'link3', 'link4'];
-    const header2 = [ 'link1', 'link2', 'link3', 'link4', 'link5'];
-    const header3 = [ 'link1', 'link2', 'link3'];
-    const header4 = [ 'link1', 'link2', 'link3', 'link4'];
+    const header1 = [ 'About', 'Jobs', 'For the Record'];
+    const header2 = [ 'For Artists', 'Developers', 'Advertising', 'Investors', 'Vendors'];
+    const header3 = [ 'Support', 'Web Player', 'Free Mobile App'];
+
 
 
 
@@ -13,13 +13,12 @@ const Footer = () => {
             <div className="footerDesktop">
                 {/* Div is divided into 5 fractions in scss file */}
                 <div className="footerDesktop__company">
-                    <p className="header">
-                        Company Name & Logo
-                    </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="800" width="1200" viewBox="-33.4974 -55.829 290.3108 334.974"><path d="M177.707 98.987c-35.992-21.375-95.36-23.34-129.719-12.912-5.519 1.674-11.353-1.44-13.024-6.958-1.672-5.521 1.439-11.352 6.96-13.029 39.443-11.972 105.008-9.66 146.443 14.936 4.964 2.947 6.59 9.356 3.649 14.31-2.944 4.963-9.359 6.6-14.31 3.653m-1.178 31.658c-2.525 4.098-7.883 5.383-11.975 2.867-30.005-18.444-75.762-23.788-111.262-13.012-4.603 1.39-9.466-1.204-10.864-5.8a8.717 8.717 0 015.805-10.856c40.553-12.307 90.968-6.347 125.432 14.833 4.092 2.52 5.38 7.88 2.864 11.968m-13.663 30.404a6.954 6.954 0 01-9.569 2.316c-26.22-16.025-59.223-19.644-98.09-10.766a6.955 6.955 0 01-8.331-5.232 6.95 6.95 0 015.233-8.334c42.533-9.722 79.017-5.538 108.448 12.446a6.96 6.96 0 012.31 9.57M111.656 0C49.992 0 0 49.99 0 111.656c0 61.672 49.992 111.66 111.657 111.66 61.668 0 111.659-49.988 111.659-111.66C223.316 49.991 173.326 0 111.657 0" fill="#fff"/></svg>
+                    Spotify
                 </div>
                 {/* column1 */}
                 <div className="footerDesktop__column">
-                    <p className="sub-header">Header 1</p>
+                    <p className="sub-header">Company</p>
                     <ul className="footerDesktop__links">
                         {header1.map((link, index) => {
                             return (
@@ -33,7 +32,7 @@ const Footer = () => {
 
                 {/* column2 */}
                 <div className="footerDesktop__column">
-                    <p className="sub-header">Header 2</p>
+                    <p className="sub-header">Communties</p>
                     <ul className="footerDesktop__links">
                         {header2.map((link, index) => {
                             return (
@@ -47,7 +46,7 @@ const Footer = () => {
 
                 {/* column3 */}
                 <div className="footerDesktop__column">
-                    <p className="sub-header">Header 3</p>
+                    <p className="sub-header">Useful Links</p>
                     <ul className="footerDesktop__links">
                         {header3.map((link, index) => {
                             return (
@@ -61,16 +60,19 @@ const Footer = () => {
 
                 {/* column4 */}
                 <div className="footerDesktop__column">
-                    <p className="sub-header">Header 4</p>
-                    <ul className="footerDesktop__links">
-                        {header4.map((link, index) => {
-                            return (
-                                <li key={index} className="footerDesktop__links-item">
-                                    {link}
-                                </li>
-                            )
-                        })}
-                    </ul>
+                        <ul className="footerDesktop__socials">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-instagram" viewBox="0 0 16 16"> <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" fill="white"></path> </svg>
+                            </li>
+
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" fill="#fff"></path> </svg>
+                            </li>
+
+                            <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" fill="#fff"></path> </svg>
+                            </li>
+                        </ul>
                 </div>
 
             </div>
@@ -80,8 +82,8 @@ const Footer = () => {
                     {/* column1 */}
                     <div className="accordion">
                     <input type="checkbox" id="first" />
-                    <label for="first" className="sub-header acc-label">Header 1</label>
-                    <div class="acc-content">
+                    <label for="first" className="sub-header acc-label">Company</label>
+                    <div className="acc-content">
                         <ul className="footerDesktop__links">
                             {header1.map((link, index) => {
                                 return (
@@ -97,7 +99,7 @@ const Footer = () => {
                     {/* column 2 */}
                     <div className="accordion">
                         <input type="checkbox" id="second" />
-                        <label for="second" className="sub-header acc-label">Header 2</label>
+                        <label for="second" className="sub-header acc-label">Communities</label>
                         <div className="acc-content">
                             <ul className="footerDesktop__links">
                                 {header2.map((link, index) => {
@@ -113,7 +115,7 @@ const Footer = () => {
                      {/* column 3 */}
                      <div className="accordion">
                         <input type="checkbox" id="third" />
-                        <label for="third" className="sub-header acc-label">Header 3</label>
+                        <label for="third" className="sub-header acc-label">Useful Links</label>
                         <div className="acc-content">
                             <ul className="footerDesktop__links">
                                 {header3.map((link, index) => {
@@ -128,19 +130,19 @@ const Footer = () => {
                     </div>
                      {/* column 4 */}
                      <div className="accordion">
-                        <input type="checkbox" id="fourth" />
-                        <label for="fourth" className="sub-header acc-label">Header 4</label>
-                        <div className="acc-content">
-                            <ul className="footerDesktop__links">
-                                {header4.map((link, index) => {
-                                    return (
-                                        <li key={index} className="footerDesktop__links-item">
-                                            {link}
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </div>
+                     <ul className="footerDesktop__socials">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-instagram" viewBox="0 0 16 16"> <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" fill="white"></path> </svg>
+                            </li>
+
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" fill="#fff"></path> </svg>
+                            </li>
+
+                            <li>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#fff" class="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" fill="#fff"></path> </svg>
+                            </li>
+                        </ul>
                     </div>
             </div>
             </div>
